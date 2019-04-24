cbe823f9d685ac131577851aa8f072f924a402d4
Gourav <gnayyar@vmware.com>
2019-04-24 07:57:08 +0530

    Initial Commit

---
+++ UserListSampleApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  UserListSampleApp
+//
+//  Created by Gourav Nayyar on 24/04/19.
+//  Copyright © 2019 Gourav Nayyar. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+

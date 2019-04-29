9597ecc3a318cef89deb526f0f142bbac94ec41e
Gourav <gnayyar@vmware.com>
2019-04-29 06:31:26 +0530

    this example outlines the additon on the background thread and print the result once all the results are received
    Use of Dispatch group

--- UserListSampleApp/ViewController.swift
+++ UserListSampleApp/ViewController.swift
@@ -12,12 +12,67 @@ class ViewController: UITableViewController {
 
     var users:[User] = []
     let dataSource = UserDataSource()
+    let dispatchGroup = DispatchGroup()
 
+    let array1 = [1,2,3,4,5]
+    let array2 = [5,6,7,8,9]
+    let array3 = [11,12,13,14,15]
+    let result: Int = 0
+
+
+    func sum(of elements: [Int], completion: @escaping(Int) -> Void) {
+        var result = 0
+        DispatchQueue.global(qos: .background).async {
+            for index in 0..<elements.count {
+                let val = elements[index]
+                result += val
+            }
+
+            let random = (0...10).random
+
+            sleep(UInt32(random))
+            completion(result)
+        }
+    }
+
+    func printSum() {
+        var output = 0
+
+        dispatchGroup.enter()
+        sum(of: array1) { (result) in
+            print("Result 1 = \(result)")
+            output += result
+            self.dispatchGroup.leave()
+        }
+
+
+        dispatchGroup.enter()
+        sum(of: array2) { (result) in
+            print("Result 2 = \(result)")
+            output += result
+            self.dispatchGroup.leave()
+        }
+
+        dispatchGroup.enter()
+        sum(of: array3) { (result) in
+            print("Result 3 = \(result)")
+            output += result
+            self.dispatchGroup.leave()
+        }
+
+        dispatchGroup.notify(queue: .main) {
+            print(output)
+        }
+
+
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
+
+        printSum()
         // Do any additional setup after loading the view, typically from a nib.
-        self.loadData()
+//        self.loadData()
     }
 
     func loadData() {
@@ -27,5 +82,22 @@ class ViewController: UITableViewController {
 
         dataSource.fetch("https://jsonplaceholder.typicode.com/users")
         tableView.dataSource = dataSource
+    }
+}
+
+extension Range where Bound == Int {
+    var random: Int {
+        return lowerBound + numericCast(arc4random_uniform(numericCast(count)))
+    }
+    func random(_ n: Int) -> [Int] {
+        return (0..<n).map { _ in random }
+    }
+}
+extension ClosedRange where Bound == Int {
+    var random: Int {
+        return lowerBound + numericCast(arc4random_uniform(numericCast(count)))
+    }
+    func random(_ n: Int) -> [Int] {
+        return (0..<n).map { _ in random }
     }
 }
